1a836139f4fb9e61a4758b546dc74eeceaba8357
Atul <atulbansal4444@gmail.com>
2021-11-08 16:22:13 +0530

    added coding challenge 4

---
+++ LearnJs/Fundamentals/Part1/codingChallenge4.js
@@ -0,0 +1,17 @@
+//data1
+let bill = 275;
+let tip = (bill >= 50 && bill <= 300) ? bill * .15 : bill * .2;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+
+//data2
+bill = 40;
+tip = (bill >= 50 && bill <= 300) ? bill * .15 : bill * .2;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+
+//data3
+bill = 430;
+tip = (bill >= 50 && bill <= 300) ? bill * .15 : bill * .2;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
