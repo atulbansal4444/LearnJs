8903eecd383a3369c8044c4494f044d69f0b99f9
Atul <atulbansal4444@gmail.com>
2021-11-08 16:24:42 +0530

    added coding challenge 4

--- LearnJs/Fundamentals/Part1/codingChallenge4.js
+++ LearnJs/Fundamentals/Part1/codingChallenge4.js
@@ -1,3 +1,17 @@
+/*
+Steven wants to build a very simple tip calculator for whenever he goes eating in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+
+Your tasks:
+1. Calculate the tip, depending on the bill value. Create a variable called 'tip'
+for this. It's not allowed to use an if/else statement 😅.
+2. Print a string to the console containing the bill value,
+the tip, and the fina lvalue (bill + tip). 
+Example: “The bill was 275, the tip was 41.25, and the total value 316.25”
+
+Test data:
+Data 1: Test for bill values 275, 40 and 430
+*/
+
 //data1
 let bill = 275;
 let tip = (bill >= 50 && bill <= 300) ? bill * .15 : bill * .2;
