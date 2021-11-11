ff417e091715004f15098df9aaab2434f14677b3
Atul <atulbansal4444@gmail.com>
2021-11-11 09:37:54 +0530

    added script for this and object understanding

--- LearnJs/Fundamentals/Part2/script.js
+++ LearnJs/Fundamentals/Part2/script.js
@@ -1,7 +1,28 @@
-'use strict';
+// 'use strict';
 
-const private = 'pvt';
-console.log(private);
+// const private = 'pvt';
+// console.log(private);
 
-x = 10;
-console.log(x);
+// x = 10;
+// console.log(x);
+
+
+const obj = {
+    firstName: "",
+    lastN: ""
+}
+
+function getFullName(firstName, lastN) {
+    this.firstName = firstName;
+    this.lastN = lastN;
+    obj.firstName = firstName;
+    obj.lastN = lastN;
+    getOutput();
+}
+
+function getOutput() {
+    console.log(this.firstName + " " + this.lastN);
+    console.log(obj.firstName + " " + obj.lastN);
+}
+
+getFullName("Atul", "Bansal");
